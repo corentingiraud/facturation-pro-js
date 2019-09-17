1f01b045440d2e84e7ecb766699db0b93bde62a8
corentingiraud <corentin.giraud@insa-lyon.fr>
2019-09-17 17:30:53 +0200

    Update interfaces

--- lib/models/item.ts
+++ lib/models/item.ts
@@ -1,13 +1,13 @@
 export interface Item {
-  id: number;
-  nature: number;
-  optional: boolean;
-  position: number;
-  product_id: string;
-  quantity: string;
-  style: string;
-  title: string;
-  total: string;
-  unit_price: string;
-  vat: string;
+  id?: number;
+  nature?: number;
+  optional?: boolean;
+  position?: number;
+  product_id?: string;
+  quantity?: string;
+  style?: string;
+  title?: string;
+  total?: string;
+  unit_price?: string;
+  vat?: string;
 }
